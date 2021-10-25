0adb20995e967554e6f1e8ae0344a0007fbdcacb
PashaRobot <pavel.bezdelev@mail.ru>
2021-10-25 15:40:18 +0700

    Задание 1 по варианту

--- Timer/Timer/main.cpp
+++ Timer/Timer/main.cpp
@@ -9,16 +9,17 @@
 #include <avr/interrupt.h>
 #include "HD44780.h"
 
-void TCD0_init(){
-	TCD0_PER = 1953;					//задаем период таймера
+void TCD0_init()
+{
+	TCD0_PER = 4199;					//задаем период таймера
 	TCD0_INTCTRLA = 3<<TC0_OVFINTLVL_gp;//задаем максимальный уровень прерывания по переполнению
-	TCD0_CTRLA = TC_CLKSEL_DIV1024_gc;	//задаем предделитель и одновременно запускаем таймер
+	TCD0_CTRLA = 0b00000111; //TC_CLKSEL_DIV1024_gc;	//задаем предделитель и одновременно запускаем таймер
 }
 
 int main(void)
 {
-	PORTD_DIRSET = 9;	//настроим на выход 2 линии порта
-	PORTD_OUTSET = 8;	//включим светодиод на одной стороне платы
+	PORTD_DIRSET = 15;	//настроим на выход 2 линии порта
+	PORTD_OUTSET = 9;	//включим светодиод на одной стороне платы
 	
 	PMIC_CTRL = PMIC_HILVLEN_bm|PMIC_MEDLVLEN_bm|PMIC_LOLVLEN_bm; // разрешаем прерывания всех уровней
 	
@@ -31,6 +32,7 @@ int main(void)
 	}
 }
 
-ISR(TCD0_OVF_vect){
-	PORTD_OUTTGL = 9; // а светодиоды переключаем по прерыванию раз в секунду
+ISR(TCD0_OVF_vect)
+{
+	PORTD_OUTTGL = 15; // а светодиоды переключаем по прерыванию раз в секунду
 }
